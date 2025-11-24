05d2680e1758418a40c6d31fdb3b13ac412e9def
rnktlhr <rinakitluhur10@gmail.com>
2025-11-24 09:06:46 +0700

    menambahkan code dan file db js pada config

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const db = require('./models');
+
+async function connectDatabase() {
+    try {
+        await db.sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
+        await db.sequelize.sync({ alter: true });
+        console.log('All models were synchronized successfully.');
+    }catch (err){
+        console.error('Unable to connect to the database:', err.massage);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDatabase;
